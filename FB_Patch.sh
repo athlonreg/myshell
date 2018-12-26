29f2b92d0e2f142a24d3142eddade780a18784ff
canvas <15563836030@163.com>
2018-12-26 22:21:46 +0800

    修正显示问题和双显卡判断问题(Credits lihaoyun6)

--- FB_Patch.sh
+++ FB_Patch.sh
@@ -11,90 +11,101 @@
 var(){
 	fbname=$(kextstat | grep -o "AppleIntel.*Framebuffer[^ ]*")
 	igid=$(ioreg -l | grep ig-platform-id | sed "s/.*<\([^>]*\)>.*/\1/g")
-	org=$(xxd -ps /S*/L*/E*/$fbname.kext/C*/M*/$fbname | tr -d '\n' | grep -Eo "${igid}0.*?00000060" | sed "s/$igid//g")
-	patch=$(echo $org | sed 's/\(.*\)00000060\(.*\)/\100000080\2/')
-	gpu=$(system_profiler SPDisplaysDataType | awk -F': ' '/^\ *Chipset Model:/ {printf $2}')
+	org=$(xxd -ps /S*/L*/E*/${fbname}.kext/C*/M*/${fbname} | tr -d '\n' | grep -Eo "${igid}0.*?00000060" | sed "s/${igid}//g")
+	patch=$(printf ${org} | sed 's/\(.*\)00000060\(.*\)/\100000080\2/')
+	gpu=$(system_profiler SPDisplaysDataType | grep "Intel" | grep -v "Vendor" | grep -v "Chipset" | sed "s/://g" | cut -b 5-)
 	osversion=$(sw_vers -productVersion)
 	shortversion=$(sw_vers -productVersion | cut -b 1,2,3,4,5)
 }
 
 # Reverse igid
 rev(){
-	ig4=$(echo $igid | cut -c 1,2) 
-	ig3=$(echo $igid | cut -c 3,4) 
-	ig2=$(echo $igid | cut -c 5,6) 
-	ig1=$(echo $igid | cut -c 7,8) 
+	ig4=$(printf ${igid} | cut -c 1,2) 
+	ig3=$(printf ${igid} | cut -c 3,4) 
+	ig2=$(printf ${igid} | cut -c 5,6) 
+	ig1=$(printf ${igid} | cut -c 7,8) 
 	igid="0x$ig1$ig2$ig3$ig4"
 }
 
 # Get patch
 highPatch(){
-	hexfind=$(echo $org | sed "s/.\{8\}/& /g")
-	hexreplace=$(echo $patch | sed "s/.\{8\}/& /g")
-	base64find=$(echo $org | xxd -r -p | base64)
-	base64replace=$(echo $patch | xxd -r -p | base64)
-	comment=$(echo "Change VRAM 1536 -> 2048 for $igid")
+	hexfind=$(printf ${org} | sed "s/.\{8\}/& /g")
+	hexreplace=$(printf $patch | sed "s/.\{8\}/& /g")
+	base64find=$(printf ${org} | xxd -r -p | base64)
+	base64replace=$(printf $patch | xxd -r -p | base64)
+	comment=$(printf "Change VRAM 1536 -> 2048 for ${igid}")
 	matchos=$(sw_vers -productVersion)
 }
 
 # Print Message
 printMsg(){
-	echo -e "\n=================================================================>"
-	echo -e "Your GPU model is: 		$gpu"
-	echo -e "Your GPU fbname is: 		$fbname"
-	echo -e "Your ig-platform-id is: 	$igid"
-	echo -e "Your macOS version is: 		$osversion"
-	echo -e "<================================================================="
-	echo -e "\nThe VRAM patch are here: "
+	printf "\033[1m\033[37;41mYour Intel HD Graphics is here ......\033[0m\n"
+	printTop
+	printf "Your GPU model is:λ${gpu}\n"
+	printf "Your GPU fbname is:λ${fbname}\n"
+	printf "Your ig-platform-id is:λ${igid}\n"
+	printf "Your macOS version is:λ${osversion}\n"
+	printBottom
+}
+
+printTop(){
+	printf "==========================>\n"
+}
+
+printBottom(){
+	printf "<==========================\n"
+}
+
+printSeparator(){
+	printf "\n---------------------------\n"
 }
 
 # Patch for hex
 hexPatch(){
-	echo -e "=================================================================>"
-	echo -e "hex format ......"
-	echo -e "Name:		$fbname"
-	echo -e "Find:		$hexfind"
-	echo -e "Replace:	$hexreplace"
-	echo -e "Comment:	$comment"
-	echo -e "MatchOS:	$matchos"
-	echo -e "<================================================================="
-	echo -e "\n"
+	printf "\033[1m\033[37;41mhex format ......\033[0m\n"
+	printTop
+	printf "Name:λ${fbname}\n"
+	printf "Find:λ${hexfind}\n"
+	printf "Replace:	λ$hexreplace\n"
+	printf "Comment:	λ${comment}\n"
+	printf "MatchOS:	λ${matchos}\n"
+	printBottom
 }
 
 # Patch for base64
 base64Patch(){
-	echo -e "=================================================================>"
-	echo -e "base64 format ......"
-	echo -e "Name:		$fbname"
-	echo -e "Find:		$base64find"
-	echo -e "Replace:	$base64replace"
-	echo -e "Comment:	$comment"
-	echo -e "MatchOS:	$matchos"
-	echo -e "<================================================================="
-	echo -e "\n"
+	printf "\033[1m\033[37;41mbase64 format ......\033[0m\n"
+	printTop
+	printf "Name:λ${fbname}\n"
+	printf "Find:λ${base64find}\n"
+	printf "Replace:	λ${base64replace}\n"
+	printf "Comment:	λ${comment}\n"
+	printf "MatchOS:	λ${matchos}\n"
+	printBottom
 }
 
 # Patch for device properties
 mojavePatch(){
-	echo -e "=================================================================>"
-	echo -e "Device Properties ......"
-	echo -e "Properties Keys\t\t\tProperties Value\t\tValue Type"
-	echo -e "-----------------------\t\t---------------------\t\t-----------"
-	echo -e "AAPL,slot-name\t\t\tPCI Express 3.0\t\t\tSTRING"
-	echo -e "framebuffer-patch-enable\t01000000\t\t\tDATA"
-	echo -e "framebuffer-unifiedmem\t\t00000080\t\t\tDATA"
-	echo -e "AAPL,ig-platform-id\t\t$igid\t\t\tDATA"
-	echo -e "framebuffer-con1-enable\t\t01000000\t\t\tDATA"
-	echo -e "hda-gfx\t\t\t\tonboard-1\t\t\tSTRING"
-	echo -e "model\t\t\t\t$gpu\tSTRING"
-	echo -e "<================================================================="
-	echo -e "\n"
+	printf "\033[1m\033[37;41mDevice Properties ......\033[0m\n"
+	printTop
+	printf "Properties KeysλProperties ValueλValue Type"
+	printf "\n---------------λ----------------λ----------\n"
+	printf "AAPL,slot-nameλPCI Express 3.0λSTRING\n"
+	printf "framebuffer-patch-enableλ01000000λDATA\n"
+	printf "framebuffer-unifiedmemλ00000080λDATA\n"
+	printf "AAPL,ig-platform-idλ${igid}λDATA\n"
+	printf "framebuffer-con1-enableλ01000000λDATA\n"
+	printf "hda-gfxλonboard-1λSTRING\n"
+	printf "modelλ${gpu}λSTRING\n"
+	printBottom
 }
 
 main(){
 	var
-	rev
+	#rev
 	printMsg
+	printf "\033[1m\033[37;41mThe VRAM patch is here: \033[0m\n"
+	printSeparator
 	if [[ $shortversion == "10.14" ]] ; then
 		mojavePatch
 	else
@@ -102,9 +113,9 @@ main(){
 		hexPatch
 		base64Patch
 	fi
-	echo -e "Enjoy, have a nice day!\n"
+	printf "Enjoy, have a nice day!\n"
 }
 
-main
+main | column -t -s "λ"
 
 exit 0
