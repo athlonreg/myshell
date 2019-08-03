85840bbd253f931add32e4b12b2ad520267c6113
athlonreg <15563836030@163.com>
2019-08-03 17:21:28 +0800

    Fix CLOVER path

--- RemoveBuiltByCloverString.sh
+++ RemoveBuiltByCloverString.sh
@@ -16,8 +16,8 @@ echo "+++++++++++                                                     ++++++++++
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" 
 
 cd ~/Desktop/ 
-cp /Volumes/EFI/CLOVER/CLOVERX64.efi ./CLOVERX64.efi 
-cp /Volumes/EFI/CLOVER/CLOVERX64.efi ./CLOVERX64-Patch.efi 
+cp /Volumes/EFI/EFI/CLOVER/CLOVERX64.efi ./CLOVERX64.efi 
+cp /Volumes/EFI/EFI/CLOVER/CLOVERX64.efi ./CLOVERX64-Patch.efi 
 perl -i -pe 's|\x42\x75\x69\x6c\x74\x20\x62\x79\x3a\x20\x20\x20\x20\x20\x43\x6c\x6f\x76\x65\x72|\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|sg' CLOVERX64-Patch.efi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" 
