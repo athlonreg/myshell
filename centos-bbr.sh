2330d83cfc5e6beac27822db0b9e1e79b15627e9
Canvas <15563836030@163.com>
2019-10-16 15:33:53 +0800

    Create centos-bbr.sh

---
+++ centos-bbr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
+grub2-set-default 0
+echo "Your computer will reboot after 5 seconds......"
+sleep 5
+reboot
