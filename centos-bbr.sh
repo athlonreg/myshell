d9be84935653002d4db618f834fc66912e6d18a0
Canvas <15563836030@163.com>
2019-10-20 00:38:45 +0800

    Update centos-bbr.sh

--- centos-bbr.sh
+++ centos-bbr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml -y
 egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
 grub2-set-default 0
