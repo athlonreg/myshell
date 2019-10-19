3eb9da6f49a0d89b393e5b1d2f5ec40e75eb506c
Canvas <15563836030@163.com>
2019-10-20 00:52:07 +0800

    Update centos-bbr.sh

--- centos-bbr.sh
+++ centos-bbr.sh
@@ -5,6 +5,12 @@ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml -y
 egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
 grub2-set-default 0
+
 echo "Your computer will reboot after 5 seconds......"
+
+echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+
 sleep 5
 reboot
