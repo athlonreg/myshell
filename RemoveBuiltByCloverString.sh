c1683fbabf0300305b3743943094c6843fa15426
Canvas <15563836030@163.com>
2019-11-09 10:18:17 +0800

    Update RemoveBuiltByCloverString.sh

--- RemoveBuiltByCloverString.sh
+++ RemoveBuiltByCloverString.sh
@@ -18,7 +18,7 @@ echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 cd ~/Desktop/ 
 cp /Volumes/EFI/EFI/CLOVER/CLOVERX64.efi ./CLOVERX64.efi 
 cp /Volumes/EFI/EFI/CLOVER/CLOVERX64.efi ./CLOVERX64-Patch.efi 
-perl -i -pe 's|\x42\x75\x69\x6c\x74\x20\x62\x79\x3a\x20\x20\x20\x20\x20\x43\x6c\x6f\x76\x65\x72|\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|sg' CLOVERX64-Patch.efi
+perl -i -pe 's|\x42\x75\x69\x6c\x74\x20\x62\x79\x3a\x20\x20\x20\x20\x20|\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|sg' CLOVERX64-Patch.efi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" 
 echo "++++++                                                               ++++++" 
